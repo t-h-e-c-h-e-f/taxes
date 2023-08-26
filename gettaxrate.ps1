f2c3958528e110e5132e6aa27a1cea5e621de62f
t-h-e-c-h-e-f <thechef@reasonableconspiracy.com>
2023-08-26 16:56:15 -0500

    Add files via upload

--- gettaxrate.ps1
+++ gettaxrate.ps1
@@ -1,6 +1,25 @@
 # Load the necessary .NET assembly for Windows Forms
 Add-Type -AssemblyName System.Windows.Forms
 
+# Function to search for tax rate and copy it to clipboard
+function SearchTaxRate {
+    $csvPath = "C:\taxes\merged.csv"
+    if (Test-Path $csvPath) {
+        $taxData = Import-Csv -Path $csvPath
+        $userZip = $textBox.Text
+        $foundEntry = $taxData | Where-Object { $_.ZipCode -eq $userZip }
+        if ($foundEntry) {
+            $taxRate = $foundEntry.EstimatedCombinedRate
+            [System.Windows.Forms.Clipboard]::SetText($taxRate)
+            [System.Windows.Forms.MessageBox]::Show("The Estimated Combined Rate for zip code $userZip is $taxRate%. This value has been copied to your clipboard.")
+        } else {
+            [System.Windows.Forms.MessageBox]::Show("Zip code $userZip not found in the CSV file.")
+        }
+    } else {
+        [System.Windows.Forms.MessageBox]::Show("The CSV file at $csvPath does not exist.")
+    }
+}
+
 # Create a new Windows Form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Find Sales Tax Rate'
@@ -18,6 +37,13 @@ $textBox = New-Object System.Windows.Forms.TextBox
 $textBox.Location = New-Object System.Drawing.Point(110, 10)
 $textBox.Width = 150
 
+# Handle the Enter key within the TextBox
+$textBox.Add_KeyDown({
+    if ($_.KeyCode -eq [System.Windows.Forms.Keys]::Enter) {
+        SearchTaxRate
+    }
+})
+
 # Create a button for submitting the form
 $button = New-Object System.Windows.Forms.Button
 $button.Text = 'Submit'
@@ -26,21 +52,7 @@ $button.Width = 150
 
 # Define what happens when the button is clicked
 $button.Add_Click({
-    $csvPath = "C:\taxes\merged.csv"
-    if (Test-Path $csvPath) {
-        $taxData = Import-Csv -Path $csvPath
-        $userZip = $textBox.Text
-        $foundEntry = $taxData | Where-Object { $_.ZipCode -eq $userZip }
-        if ($foundEntry) {
-            $taxRate = $foundEntry.EstimatedCombinedRate
-            [System.Windows.Forms.Clipboard]::SetText($taxRate)
-            [System.Windows.Forms.MessageBox]::Show("The Estimated Combined Rate for zip code $userZip is $taxRate%. This value has been copied to your clipboard.")
-        } else {
-            [System.Windows.Forms.MessageBox]::Show("Zip code $userZip not found in the CSV file.")
-        }
-    } else {
-        [System.Windows.Forms.MessageBox]::Show("The CSV file at $csvPath does not exist.")
-    }
+    SearchTaxRate
 })
 
 # Add controls to the form
